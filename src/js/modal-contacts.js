873fcdaad3aa467eefe6d56bcfb229b45a85cf68
mkhomenko2212 <mkhomenko2212@gmail.com>
2022-01-16 23:13:50 +0200

    модально окно

---
+++ src/js/modal-contacts.js
@@ -0,0 +1,30 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-location]'),
+    closeModalBtn: document.querySelector('[data-modal-close-location]'),
+    modal: document.querySelector('[data-modal-location]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-franchise]'),
+    closeModalBtn: document.querySelector('[data-modal-close-franchise]'),
+    modal: document.querySelector('[data-modal-franchise]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
