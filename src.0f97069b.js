e2871cdaaa56b4bab763de223fdb45a31d5e634d
Rostyslav-Baryshok <85187900+Rostyslav-Baryshok@users.noreply.github.com>
2022-01-19 08:48:40 +0000

    Deploying to gh-pages from @ Rostyslav-Baryshok/ice-cream-project@90e979889d78da06839f722332cbbbc16aeadf9e 🚀

--- src.0f97069b.js
+++ src.0f97069b.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("animate.css");
 },{"./sass/main.scss":"clu1","animate.css":"TOx8"}]},{},["Focm"], null)
-//# sourceMappingURL=/ice-cream-project/src.c9fd9bee.js.map
+//# sourceMappingURL=/ice-cream-project/src.0f97069b.js.map
